5026262f9e393cb3d7b83baa278892a347b3be3a
Wellington Cruz <marcelinoc.wellington@gmail.com>
2023-09-18 21:26:03 -0300

    Resolvido desafio proposto de criar conta em banco.

--- desafio-conta/conta-banco/src/ContaTerminal.java
+++ desafio-conta/conta-banco/src/ContaTerminal.java
@@ -1,7 +1,30 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
 
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+       public static void main(String[] args) {
+        //criando o objeto scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Para acessar sua conta, informe os dados: \n");
+
+        System.out.println("Informe seu nome: ");
+        String nome = scanner.next();
+
+        System.out.println("Informe sua agência: ");
+        String agencia = scanner.next();
+
+        System.out.println("Informe sua conta: ");
+        int conta = scanner.nextInt();
+
+        double saldo = 5000.000;
+
+
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco," +
+                " sua agência é " + agencia + ", conta " + conta + " e seu saldo de R$" + saldo + " já está disponível para saque");
+
+
     }
     
 }
